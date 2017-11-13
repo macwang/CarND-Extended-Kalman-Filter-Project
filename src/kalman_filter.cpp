5b1e66811380332fbe07e55c3ccc9ff5c53645e1
Mac Wang <macwang1@gmail.com>
2017-11-13 13:32:33 +0800

    add UpdateEKF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -59,4 +59,32 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  // From c14
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  VectorXd h = VectorXd(3);
+  float rho = sqrt(px*px + py*py);
+  float phi = atan2(py, px);
+  float rho_dot = (px*vx + py*vy)/rho;
+
+  h << rho, phi, rho_dot;
+
+  VectorXd y = z - h;
+
+  // From C7, C13
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
